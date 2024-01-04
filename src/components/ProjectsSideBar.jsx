07e024e2443a0ff37cdd63455806e89d1edd1526
cristian-corbalan <ccorbalandw@gmail.com>
2024-01-04 18:39:06 -0300

    Create ProjectsSideBar component

---
+++ src/components/ProjectsSideBar.jsx
@@ -0,0 +1,12 @@
+export default function ProjectsSideBar() {
+  return (
+    <aside>
+      <h2>Your projects</h2>
+      <button>
+        + Add Project
+      </button>
+
+      <ul></ul>
+    </aside>
+  )
+}

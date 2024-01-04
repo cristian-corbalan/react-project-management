f0f0dafa8348a3e2458bc66aee4147ca9e634851
cristian-corbalan <ccorbalandw@gmail.com>
2024-01-04 18:48:25 -0300

    Create a main and include the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import ProjectsSideBar from './components/ProjectsSideBar.jsx';
+
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen pt-8">
+      <ProjectsSideBar/>
+    </main>
   );
 }
 

669e6429a9f8f7fab591b0659efc5544dc394c1a
cristian-corbalan <ccorbalandw@gmail.com>
2024-01-04 19:11:41 -0300

    Create Input.jsx styles

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,24 @@
 export default function Input({id, label, textarea, labelHidden = false, ...props}) {
 
+  let labelClass = 'text-sm font-bold uppercase text-stone-500';
+  if (labelHidden) {
+    labelClass += ' sr-only';
+  }
+
+  let inputClass = 'w-full p-1 border-2 rounded-sm border-stone-300 bg-stone-200 text-stone-700 focus:outline-none focus:border-stone-600'
+
   return (
-    <div>
-      <label htmlFor={id} className={labelHidden ? 'sr-only' : undefined}>{label}</label>
+    <div className="flex flex-col gap-1 my-4">
+      <label htmlFor={id}
+             className={labelClass}
+      >
+        {label}
+      </label>
 
       {
         textarea
-          ? <textarea id={id} {...props}></textarea>
-          : <input id={id} {...props} />
+          ? <textarea className={inputClass} id={id} {...props}></textarea>
+          : <input className={inputClass} id={id} {...props} />
       }
     </div>
   )
